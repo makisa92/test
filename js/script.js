ef3cacf33f39b6a852d52401553cc7c1f16f10b6
makisa92 <marijarafailovic92@gmail.com>
2016-11-02 00:08:17 +0100

    First commit

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ var modal = document.getElementById('myModal');
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
 btn.onclick = function() {
@@ -13,9 +13,9 @@ btn.onclick = function() {
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -23,3 +23,12 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+// var step1 = document.getElementById("step1");
+// var nextStep2 = document.getElementById("nextStep2");
+
+// nextStep2.onclick = function() {
+// 	step1.display = "none";
+// 	step2.display = "block";
+// }
+
