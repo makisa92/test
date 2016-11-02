fd22186b6a3f51c7c6e7c2fa9f317cb9230f3823
nikola <nikola@nikola>
2016-11-02 17:01:24 +0100

    Chenges

--- js/script.js
+++ js/script.js
@@ -1,28 +1,71 @@
-// Get the modal
-var modal = document.getElementById('myModal');
+$( document ).ready(function() {
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+    // $.ajax({
+    //     dataType: "json",
+    //     crossOrigin: true,
+    //     url: 'http://survey.quantox.tech/survey',
+    //     // headers: {'Access-Control-Allow-Origin': '*', 'api-key':'4fd9b42566b547562be9f75784ad58f868e90d49'},
+    //     beforeSend: function(xhr){xhr.setRequestHeader('api-key', '4fd9b42566b547562be9f75784ad58f868e90d49');},
+    //     success: function (data) {
+    //         console.log(data);
+    //     }
+    // });
 
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
+    $.ajax({
+        type: 'GET',
+        url: 'http://www.survey.quantox.tech/survey',
+        headers: {'Access-Control-Allow-Origin': '*', 'api-key':'4fd9b42566b547562be9f75784ad58f868e90d49'},
+        crossOrigin: true,
+    });
 
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+    // Get the modal
+    var modal = document.getElementById('myModal');
 
-// When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//     modal.style.display = "none";
-// }
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    //When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
         modal.style.display = "none";
     }
-}
+
+
+    function validateEmail(email) {
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+
+    function validate() {
+        $("#result").text("");
+        var email = $("#email").val();
+        if (validateEmail(email)) {
+            $("#result").text(email + " is valid :)");
+            $("#result").css("color", "green");
+        } else {
+            $("#result").text(email + " is not valid :(");
+            $("#result").css("color", "red");
+        }
+        return false;
+    }
+
+    $("form").bind("submit", validate);
+
+
+});
+// When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
 
 // var step1 = document.getElementById("step1");
 // var nextStep2 = document.getElementById("nextStep2");
