4fee6f7360e3e80c77b2975efe079837c274d6a9
makisa92 <marijarafailovic92@gmail.com>
2016-11-14 00:06:48 +0100

    Almost done, need little more style to done, success and error message...

--- js/script.js
+++ js/script.js
@@ -1,16 +1,54 @@
+// on document ready
 $( document ).ready(function() {
 
+    // On button click show next question
     nextQuestion = function (divID) {
         $(divID).css("display","none");
         $(divID).next().css("display","block");
     }
 
+    // On button click show previus question
      prevQuestion = function (divID) {
         $(divID).css("display","none");
         $(divID).prev().css("display","block");
     }
 
+    // submit answers
+    submitAnswers = function (divID) {
+        var answers = [];
+        var textAreas = $('.textAreasCommonClass');
 
+        for (var i=0; i < textAreas.length; i++) {
+            answers.push(
+                {
+                    question_id: parseInt(textAreas[i].id.split("_").pop()),
+                    answer: textAreas[i].value
+                }
+            );
+        }
+
+        // var test = answers;
+        // send textareas values to api (if succes show message, if error show message)
+        $.ajax({
+              type: 'POST',
+              url: "http://survey.quantox.tech/answers",
+              data: answers,
+              dataType: "text",
+              success: function() { 
+                    $(divID).css("display","none");
+                    $('#success').css("display","block");
+
+              },
+              error: function(){ 
+                    $(divID).css("display","none");
+                     $('#error').css("display","block");
+        
+              }
+        });
+    }
+
+
+    // get json file
     $.ajax({
         dataType: "json",
         crossOrigin: true,
@@ -19,74 +57,70 @@ $( document ).ready(function() {
         // headers: {'Access-Control-Allow-Origin': '*', 'api-key':'4fd9b42566b547562be9f75784ad58f868e90d49'},
         // beforeSend: function(xhr){xhr.setRequestHeader('api-key', '08f1a4ed10cfe271fcbc477542c314eb6e58d51a');},
         beforeSend: function(xhr){xhr.setRequestHeader('ZUMO-API-VERSION', '2.0.0');},
-         success: function (data) {
+        success: function (data) {
 
-                    // for each user
-                    for (var i=0; i < data.length; i++) {
-                        if (i == data.length -1){
-                          $(".right-container-side").append("<div class='common' id='step_"+data[i].id+"' style='display:none'>" +
-                            "<h4 class='id'>" + data[i].id + "</h4>" +
-                            "<p class='question'>" + data[i].question + "</p>" +
-                            "<p class='category'>" + data[i].category + "</p>" +
-                            "<textarea id='answer_"+data[i].id+"' rows='4' cols='50'>gfdgdfgfdg</textarea>"+
-                            "<button onclick='prevQuestion(step_"+data[i].id+")'>Prev</button>"+
-                            "</div>");
-                        }else{
+            // for each question
+            for (var i=0; i < data.length; i++) {
 
-                        $(".right-container-side").append("<div class='common' id='step_"+data[i].id+"' style='display:none'>" +
-                            "<h4 class='id'>" + data[i].id + "</h4>" +
-                            "<p class='question'>" + data[i].question + "</p>" +
-                            "<p class='category'>" + data[i].category + "</p>" +
-                            "<textarea id='answer_"+data[i].id+"' rows='4' cols='50'>gfdgdfgfdg</textarea>"+
-                            "<button onclick='prevQuestion(step_"+data[i].id+")'>Prev</button>"+
-                            "<button onclick='nextQuestion(step_"+data[i].id+")'>Next</button>"+
-                            "</div>");
-                        }
-                        
-                    };
-                },
+                // except last one
+                if (i == data.length -1){
+
+                  $(".right-container-side").append("<div class='common' id='step_"+data[i].id+"' style='display:none'>" +
+                    "<header class='header'><p class='modul'>Modul/" + "<span>" + data[i].id  + "</span>" + "</p>" +
+                    "<p class='category'>" + data[i].category + "</p></header>" +
+                    "<p class='question'>" + data[i].question + "</p>" +
+                    "<textarea placeholder='Enter your answer here...' class='textAreasCommonClass' id='answer_"+data[i].id+"'></textarea>"+
+                    "<footer>"+
+                    "<img src='images/leftArrow.png' class='leftArrow' onclick='prevQuestion(step_"+data[i].id+")'/>"+
+                    "<button class='finish' onclick='submitAnswers(step_"+data[i].id+")'>Finish</button>"+
+                    "</footer>"+
+                    "</div>");
+
+                }else{
+
+                $(".right-container-side").append("<div class='common' id='step_"+data[i].id+"' style='display:none'>" +
+                    "<header class='header'><p class='modul'>Modul/" + "<span>" + data[i].id  + "</span>"  + "</p>" +
+                    "<p class='category'>" + data[i].category + "</p></header>" +
+                    "<p class='question'>" + data[i].question + "</p>" +
+                    "<textarea placeholder='Enter your answer here...' class='textAreasCommonClass' id='answer_"+data[i].id+"'></textarea>"+
+                    "<footer>"+
+                    "<img src='images/leftArrow.png' class='leftArrow' onclick='prevQuestion(step_"+data[i].id+")'/>"+
+                    "<img src='images/rightArrow.png' class='rightArrow' onclick='nextQuestion(step_"+data[i].id+")'/>"+
+                     "</footer>"+
+                    "</div>");
+                }
+                
+            };
+        },
     });
 
     
 
 
-
-    // $.ajax({
-    //     type: 'GET',
-    //     url: 'http://www.survey.quantox.tech/survey',
-    //     headers: {'Access-Control-Allow-Origin': '*', 'api-key':'08f1a4ed10cfe271fcbc477542c314eb6e58d51a'},
-    //     crossOrigin: true,
-    // });
-
-
     // Get the modal
-    var modal = document.getElementById('myModal');
+    var startModal = document.getElementById('startTest');
+    var startBtn = document.getElementById("startBtn");
+    var close = document.getElementsByClassName("close")[0];
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks the button, open the modal
-    btn.onclick = function() {
-        modal.style.display = "block";
+    startBtn.onclick = function() {
+        startModal.style.display = "block";
     }
 
-    //When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
+    close.onclick = function() {
+        startModal.style.display = "none";
     }
 
-
+    // validate email using regex
     function validateEmail(email) {
         var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(email);
     }
 
     function validate() {
+
         $("#result").text("");
         var email = $("#email").val();
+
         if (validateEmail(email)) {
         
             $("#validate").click(function(event) {
@@ -106,6 +140,8 @@ $( document ).ready(function() {
     $("form").bind("submit", validate);
 
 });
+
+
 // When the user clicks anywhere outside of the modal, close it
 // window.onclick = function(event) {
 //     if (event.target == modal) {
