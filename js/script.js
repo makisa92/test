d2deb689774ea460be0267c72e8e0b26c5f267dc
makisa92 <marijarafailovic92@gmail.com>
2016-11-14 23:25:57 +0100

    Fixes...

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 // on document ready
 $( document ).ready(function() {
 
+     // Get the modal
+    var startModal = document.getElementById('startTest');
+    var startBtn = document.getElementById("startBtn");
+    var close = document.getElementsByClassName("close")[0];
+
+    startBtn.onclick = function() {
+        startModal.style.display = "block";
+    }
+
+    close.onclick = function() {
+        startModal.style.display = "none";
+    }
+
     // On button click show next question
     nextQuestion = function (divID) {
         $(divID).css("display","none");
@@ -33,7 +46,7 @@ $( document ).ready(function() {
               type: 'POST',
               url: "http://survey.quantox.tech/answers",
               data: answers,
-              dataType: "text",
+              dataType: "json",
               success: function() { 
                     $(divID).css("display","none");
                     $('#success').css("display","block");
@@ -53,10 +66,10 @@ $( document ).ready(function() {
         dataType: "json",
         crossOrigin: true,
         // url: 'http://survey.quantox.tech/survey',
-        url:'https://dentist-reminder.azurewebsites.net/api/Questions',
+        url:'http://survey.quantox.tech/survey',
         // headers: {'Access-Control-Allow-Origin': '*', 'api-key':'4fd9b42566b547562be9f75784ad58f868e90d49'},
         // beforeSend: function(xhr){xhr.setRequestHeader('api-key', '08f1a4ed10cfe271fcbc477542c314eb6e58d51a');},
-        beforeSend: function(xhr){xhr.setRequestHeader('ZUMO-API-VERSION', '2.0.0');},
+        beforeSend: function(xhr){xhr.setRequestHeader('api-key', '1d8ab00b433b2d3fe15134b3e1cd03da1a8eefa9');},
         success: function (data) {
 
             // for each question
@@ -78,38 +91,23 @@ $( document ).ready(function() {
 
                 }else{
 
-                $(".right-container-side").append("<div class='common' id='step_"+data[i].id+"' style='display:none'>" +
-                    "<header class='header'><p class='modul'>Modul/" + "<span>" + data[i].id  + "</span>"  + "</p>" +
-                    "<p class='category'>" + data[i].category + "</p></header>" +
-                    "<p class='question'>" + data[i].question + "</p>" +
-                    "<textarea placeholder='Enter your answer here...' class='textAreasCommonClass' id='answer_"+data[i].id+"'></textarea>"+
-                    "<footer>"+
-                    "<img src='images/leftArrow.png' class='leftArrow' onclick='prevQuestion(step_"+data[i].id+")'/>"+
-                    "<img src='images/rightArrow.png' class='rightArrow' onclick='nextQuestion(step_"+data[i].id+")'/>"+
-                     "</footer>"+
-                    "</div>");
-                }
+                    $(".right-container-side").append("<div class='common' id='step_"+data[i].id+"' style='display:none'>" +
+                        "<header class='header'><p class='modul'>Modul/" + "<span>" + data[i].id  + "</span>"  + "</p>" +
+                        "<p class='category'>" + data[i].category + "</p></header>" +
+                        "<p class='question'>" + data[i].question + "</p>" +
+                        "<textarea placeholder='Enter your answer here...' class='textAreasCommonClass' id='answer_"+data[i].id+"'></textarea>"+
+                        "<footer>"+
+                        "<img src='images/leftArrow.png' class='leftArrow' onclick='prevQuestion(step_"+data[i].id+")'/>"+
+                        "<img src='images/rightArrow.png' class='rightArrow' onclick='nextQuestion(step_"+data[i].id+")'/>"+
+                         "</footer>"+
+                        "</div>");
+                    }
                 
             };
         },
     });
 
-    
-
-
-    // Get the modal
-    var startModal = document.getElementById('startTest');
-    var startBtn = document.getElementById("startBtn");
-    var close = document.getElementsByClassName("close")[0];
-
-    startBtn.onclick = function() {
-        startModal.style.display = "block";
-    }
-
-    close.onclick = function() {
-        startModal.style.display = "none";
-    }
-
+   
     // validate email using regex
     function validateEmail(email) {
         var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
